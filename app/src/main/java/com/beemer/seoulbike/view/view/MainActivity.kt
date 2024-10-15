3bd67c5ba8d2594548ca69efa66e2ad89e863608
caadiq <caadiq@gmail.com>
2024-10-15 15:03:13 +0900

    앱 기본 설정

---
+++ app/src/main/java/com/beemer/seoulbike/view/view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.beemer.seoulbike.view.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.beemer.seoulbike.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+}
